1922ab4995e5ca13c5f0dad338ced28bd8eabb0c
coreycosman <cosman.corey@gmail.com>
2018-12-04 20:54:13 +0400

    refactor

--- public/js/queryStrings.js
+++ public/js/queryStrings.js
@@ -1,34 +1,13 @@
 const search = window.location.search.toString();
 
 if (search === "?=%22description%22") {
-  $.get('/aboutme/description')
-    .then(data => $(".description").replaceWith(data))
-    .then(() => $("#btn-1").hide())
-    .catch(e => console.log(e))
-  }
-  else if (search === "?=%22tech%22") {
-    $.get('/aboutme/tech')
-      .then(data => $(".tech").replaceWith(data))
-      .then(() => $("#btn-2").hide())
-      .catch(e => console.log(e))
-
-  } else if (search === "?=%22techstack%22") {
-    $.get('/aboutme/stack')
-      .then(data => $(".stack").replaceWith(data))
-      .then(() => $("#btn-3").hide())
-      .catch(e => console.log(e))
-
-  } else if (search === "?=%22hobbies%22") {
-    $.get('/aboutme/hobbies')
-      .then(data => $(".hobbies").replaceWith(data))
-      .then(() => $("#btn-4").hide())
-      .catch(e => console.log(e))
-
-  } else if (search != '') {
-    $.get('/aboutme/all')
-      .then(data => $(".description").replaceWith(data))
-      .then(() => {
-        $("#btn-1, #btn-2, #btn-3, #btn-4").hide();
-      })
-      .catch(e => console.log(e))
-  }
+  window.location.replace("/aboutme/description");
+} else if (search === "?=%22tech%22") {
+  window.location.replace("/aboutme/tech");
+} else if (search === "?=%22techstack%22") {
+  window.location.replace("/aboutme/stack");
+} else if (search === "?=%22hobbies%22") {
+  window.location.replace("/aboutme/hobbies");
+} else if (search != "") {
+  window.location.replace("/aboutme/all");
+}
--- server/router.js
+++ server/router.js
@@ -1,20 +1,19 @@
 // IMPORTS:
-const rootController = require('./controllers/root-controller');
-const postsController = require('./controllers/posts-controller');
-const aboutsController = require('./controllers/abouts-controller');
+const rootController = require("./controllers/root-controller");
+const postsController = require("./controllers/posts-controller");
+const aboutsController = require("./controllers/abouts-controller");
 
 // ROUTER:
-module.exports = (app) => {
+module.exports = app => {
   // root
-  app.get('/', rootController.root)
-  app.get('/test', rootController.test)
+  app.get("/", rootController.root);
   // posts controller routes
-  app.get('/posts', postsController.posts)
+  app.get("/posts", postsController.posts);
   // abouts controller routes
-  app.get('/aboutme', aboutsController.index)
-  app.get('/aboutme/all', aboutsController.all)
-  app.get('/aboutme/description', aboutsController.description)
-  app.get('/aboutme/tech', aboutsController.tech)
-  app.get('/aboutme/stack', aboutsController.stack)
-  app.get('/aboutme/hobbies', aboutsController.hobbies)
-}
+  app.get("/aboutme", aboutsController.index);
+  app.get("/aboutme/all", aboutsController.all);
+  app.get("/aboutme/description", aboutsController.description);
+  app.get("/aboutme/tech", aboutsController.tech);
+  app.get("/aboutme/stack", aboutsController.stack);
+  app.get("/aboutme/hobbies", aboutsController.hobbies);
+};
